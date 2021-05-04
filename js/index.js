65191d3a99739e1b72e702f13788e9296dace107
dominthebox <dsallustro@gmail.com>
2021-05-04 18:41:49 -0400

    figured out the nav listeners

--- js/index.js
+++ js/index.js
@@ -1,10 +1,18 @@
 // Your code goes here
 
-const header = document.querySelector('.main-navigation');
 const nav = document.querySelector('.nav');
 
 nav.addEventListener('mouseover', (event) => {
     event.target.style.color = 'coral';
-})
+    setTimeout(function() {
+        event.target.style.color = "";
+      }, 500);
+    }, false);
 
-header.addEventListener('')
+nav.addEventListener('focus', (event) => {
+    event.target.style.background = 'pink';
+}, true);
+
+nav.addEventListener('blur', (event) => {
+    event.target.style.background = '';
+  }, true);
